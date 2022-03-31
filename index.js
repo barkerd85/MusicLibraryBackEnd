4f16bbdc84d291d168657cc4f092d48e5a5a2981
Dustin Barker <barkerd85@yahoo.com>
2022-03-31 17:42:32 -0600

    Added validation function

---
+++ index.js
@@ -0,0 +1,64 @@
+// Imports
+const express = require("express");
+const songValidate = require("./middleware/song-validation");
+const repoContext = require("./repository/repository-wrapper");
+
+const app = express()
+
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+// Endpoints
+// http://localhost:5000 (BASE URL)
+// http://localhost:5000/api/songs
+
+// Starting a Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running!: On PORT: ${PORT}`);
+});
+
+// Endpoints
+
+
+// GET all songs
+app.get("/api/songs", (req, res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+});
+
+
+
+// GET song by id
+// http://localhost:5000/api/songs/:id
+app.get("/api/songs/:id", [songValidate], (req, res) => {
+    const id = req.params.id;
+    const song = repoContext.songs.findSongById(id);
+    return res.send(song);
+});
+
+// POST new song
+app.post("/api/songs",  (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.status(201).send(addedSong);
+
+});
+
+
+// PUT existing song
+app.put("/api/songs/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const songPropertiesToModify = req.body;
+    const songToUpdate = repoContext.songs.updateSong(id, songPropertiesToModify);
+    return res.send(songToUpdate);
+});
+
+// Delete song
+app.delete("/api/songs/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const deletedSong = repoContext.songs.deleteSong(id);
+    return res.send(deletedSong);
+})
---
+++ middleware/song-validation.js
@@ -0,0 +1,25 @@
+const { songs } = require("../repository/repository-wrapper");
+
+function songValidate(req, res, next) {
+    let songs = req.body;
+    let properties = [
+        { name: "title", type: "string" },
+        { name: "album", type: "string" },
+        { name: "artist", type: "string" },
+        { name: "genre", type: "string" },
+        { name: "releaseDate", type: "number" },
+    ];
+    for (const property of properties) {
+        if (
+            songs.hasOwnProperty(property.name) &&
+            typeof (songs[property.name] === property.type)
+        ) {
+    continue;
+}   else {
+    return res.status(403).send(`Song body not valid!`);
+}
+    }
+return next();
+}
+
+module.exports = songValidate;
